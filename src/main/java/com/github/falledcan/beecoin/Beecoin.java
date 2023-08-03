24d8888f49233c45e1cbaf4aeeac1a8c57e48c83
落ちていたビール缶 <67153833+FalledCan@users.noreply.github.com>
2023-08-03 20:37:15 +0900

    economy

--- src/main/java/com/github/falledcan/beecoin/Beecoin.java
+++ src/main/java/com/github/falledcan/beecoin/Beecoin.java
@@ -1,17 +1,37 @@
 package com.github.falledcan.beecoin;
 
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public final class Beecoin extends JavaPlugin {
 
-    @Override
+    public static Beecoin getInstance;
+    public EconomyImplementer economyImplementer;
+    private VaultHook vaultHook;
+
+    public final HashMap<UUID,Double> playerBank = new HashMap<>();
+
     public void onEnable() {
-        // Plugin startup logic
-
+        runOnEnable();
     }
 
-    @Override
+    private void instanceClasses() {
+        getInstance = this;
+        economyImplementer = new EconomyImplementer();
+        vaultHook = new VaultHook();
+    }
+
+    public void runOnEnable(){
+        instanceClasses();
+        vaultHook.hook();
+        this.getCommand("econo").setExecutor(new EconomyCommands());
+    }
+
     public void onDisable() {
-        // Plugin shutdown logic
+        vaultHook.unhook();
     }
+
 }
